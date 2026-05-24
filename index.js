3220718d45704fb1c0980fb47e210e37db7fdf1a
bluegalaxian <realbluegalaxian@gmail.com>
2026-05-24 21:13:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const mineflayer = require('mineflayer')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('AFK Bot Running')
+})
+
+app.listen(process.env.PORT || 3000)
+
+const bot = mineflayer.createBot({
+  host: 'YOUR_SERVER_IP',
+  port: 25565,
+  username: 'AFK_Bot',
+  auth: 'offline'
+})
+
+bot.on('spawn', () => {
+  console.log('Bot joined server!')
+
+  setInterval(() => {
+
+    // Jump
+    bot.setControlState('jump', true)
+
+    setTimeout(() => {
+      bot.setControlState('jump', false)
+    }, 500)
+
+    // Random camera movement
+    bot.look(
+      Math.random() * Math.PI * 2,
+      Math.random() * Math.PI / 2,
+      true
+    )
+
+  }, 30000)
+})
+
+bot.on('error', err => console.log(err))
+bot.on('end', () => console.log('Disconnected'))
